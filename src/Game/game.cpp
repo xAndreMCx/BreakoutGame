364b599ea8e63ed53063984503319ed5a981940e
Andre du Toit <xandremcx@gmail.com>
2022-11-05 14:02:58 +0200

    quick setup

---
+++ src/Game/game.cpp
@@ -0,0 +1,11 @@
+#include "Game/game.h"
+
+namespace Game {
+Game::Game(unsigned int width, unsigned int height) {}
+Game::~Game() {}
+
+void Game::Init() {}
+void Game::ProcessInput(float deltaTime) {}
+void Game::Update(float deltaTime) {}
+void Game::Render() {}
+} // namespace Game
