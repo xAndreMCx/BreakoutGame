f62aea732f2cbc3e11ec74ce575733501716ec21
Andre du Toit <xandremcx@gmail.com>
2022-11-08 13:25:51 +0200

    created level setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include <GLAD/gl.h>
 #include <GLFW/glfw3.h>
 
-const GLuint WIDTH = 800, HEIGHT = 600;
+#include <iostream>
+
+#include "Game/game.h"
+#include "OpenGL/debug.h"
+#include "resource_manager.h"
+
+const unsigned int WIDTH = 800;
+const unsigned int HEIGHT = 600;
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
   if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) glfwSetWindowShouldClose(window, GL_TRUE);
 }
-
+Game::Game Breakout(WIDTH, HEIGHT);
 int main(void) {
   glfwInit();
 
@@ -14,23 +21,47 @@ int main(void) {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "[glad] GL with GLFW", NULL, NULL);
+  GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Breakout", NULL, NULL);
   glfwMakeContextCurrent(window);
 
   glfwSetKeyCallback(window, key_callback);
 
   int version = gladLoadGL(glfwGetProcAddress);
-  // printf("GL %d.%d\n", GLAD_VERSION_MAJOR(version), GLAD_VERSION_MINOR(version));
+  if (version == 0) {
+    std::cerr << "Failed to initialize OpenGL contex" << std::endl;
+  }
+  // TODO: move OpenGL calls out to a other file
+  GLCallV(glViewport(0, 0, WIDTH, HEIGHT));
+  GLCallV(glEnable(GL_BLEND));
+
+  GLCallV(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
+  Breakout.Init();
+
+  float deltaTime = 0.0f;
+  float lastFrame = 0.0f;
+
 
   while (!glfwWindowShouldClose(window)) {
+    float currentFrame = glfwGetTime();
+    deltaTime = currentFrame - lastFrame;
+    lastFrame = currentFrame;
     glfwPollEvents();
 
-    glClearColor(0.3f, 0.1f, 0.7f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
+    Breakout.ProcessInput(deltaTime);
+
+    Breakout.Update(deltaTime);
+
+    GLCallV(glClearColor(0.3f, 0.1f, 0.7f, 1.0f));
+    GLCallV(glClear(GL_COLOR_BUFFER_BIT));
+
+    Breakout.Render();
 
     glfwSwapBuffers(window);
   }
 
+  // TODO: should maybe distroy resource manager here
+  ResourceManager::Clear();
   glfwTerminate();
 
   return 0;
